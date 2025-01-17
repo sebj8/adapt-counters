7f391cf0bf31d5bc6d79d86a147e063a86c34aa1
sebj8 <32302148+sebj8@users.noreply.github.com>
2025-01-17 14:18:18 +0100

    Add files via upload

--- js/CountersView.js
+++ js/CountersView.js
@@ -1,4 +1,5 @@
 import ComponentView from 'core/js/views/componentView';
+import { isNaN, isNull } from 'underscore';
 
 class CountersView extends ComponentView {
 
@@ -16,27 +17,84 @@ class CountersView extends ComponentView {
 
   animateItems () {
     const speed = this.model.get('_transitionSpeed');
-    for (const item of this.model.getChildren()) {
-      const updateCount = () => {
-        const _countEnd = item.get('_countEnd');
-        const _countStart = item.get('_countStart');
-        const inc = _countEnd / speed;
 
-        if (_countStart < _countEnd) {
-          item.set('_countStart', Math.ceil(_countStart + inc));
-          setTimeout(updateCount, 1);
-        } else {
-          item.set('_countStart', _countEnd);
-          item.toggleVisited();
-        }
-      };
-      updateCount();
+    for (const item of this.model.getChildren()) {
+
+      const _countEnd = item.get('_countEnd');
+      const _countStart = item.get('_countStart');
+
+      const _startInteger = parseInt(_countStart, 10); //take integer part from start and end
+      const _endInteger = parseInt(_countEnd, 10); 
+
+      const decimalStartPartString = _countStart.toString().split('.')[1]; //same for decimal
+      const decimalEndPartString = _countEnd.toString().split('.')[1];
+
+      var _startDecimal = parseInt(decimalStartPartString, 10); //we will set it bey defualt to 0 if there is _EndDecimal
+      const _endDecimal = parseInt(decimalEndPartString, 10); 
+     
+      this.animateNumber(_startInteger, _endInteger, speed, item, (value) => {
+        item.set('_countInteger', value);
+        this.formatNumber(item);
+      });
+
+      if(_endDecimal && isNaN(_startDecimal)) //if _endDecimal is set but start decimal is not
+        _startDecimal = 0;
+
+      if (_endDecimal) { //only start if there is _endDecimal
+        this.animateNumber(_startDecimal, _endDecimal, speed, item, (value) => {
+          item.set('_countDecimal', value);
+          this.formatNumber(item);
+        });
+      }
+      else item.set('_countDecimal', null);   
     }
+  }
+
+  /**
+   * Call setReadyStatus when animation is finished
+   */
+
+
+  formatNumber(item) {
+    const countInteger = item.get('_countInteger');
+    const countDecimal = item.get('_countDecimal');
+
+    var integerPart = countInteger.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' '); //add spaces for better look
+
+    let result;
+
+    if (countDecimal)
+      result = integerPart + '.' + countDecimal.toString();
+    else
+      result = integerPart;
+
+    item.set('_delimitedString', result);  
+  }
+
+  animateNumber(start, end, duration, item, callback) {
+    const startTime = performance.now();
+    const totalChange = end - start;
+    
+    function update() {
+        const currentTime = performance.now();
+        const elapsedTime = currentTime - startTime;
+        const progress = Math.min(elapsedTime / duration, 1);
+        const currentValue = start + totalChange * progress;
+
+        callback(Math.round(currentValue));
+
+        if (progress < 1) {
+          window.requestAnimationFrame(update); 
+        }
+        else {
+          item.set('_completed', true);
+        }
+    }
+    window.requestAnimationFrame(update);
   }
 
   checkIfOnScreen({ currentTarget }, { percentInviewVertical }) {
     if (percentInviewVertical < this.model.get('_percentInviewVertical')) return;
-
     $(currentTarget).off('onscreen.animate');
     this.animateItems();
   }
@@ -46,6 +104,8 @@ class CountersView extends ComponentView {
     super.remove();
   }
 }
+
 CountersView.template = 'counters.jsx';
 
 export default CountersView;
+
